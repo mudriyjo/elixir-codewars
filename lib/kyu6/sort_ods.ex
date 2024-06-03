811ee5f3478a05e37e5ea57ff9069ff53446952d
mudriyjo <mudriyjo@gmail.com>
2024-06-04 01:47:42 +0400

    finish sort odd numbers solution

--- lib/kyu6/sort_ods.ex
+++ lib/kyu6/sort_ods.ex
@@ -1,7 +1,25 @@
 defmodule Kyu6.SortOds do
 
+  def is_odd?(el) do
+    if el == 0 do
+      false
+    else
+      rem(el, 2) != 0
+    end
+  end
   def sort_the_odd(ls) do
-    # TODO: working solution
-    ls
+    sorted_odds = ls
+    |> Enum.filter(&is_odd?/1)
+    |> Enum.sort
+
+    Enum.reduce(ls, {[], sorted_odds}, fn el, acc ->
+      res = elem(acc,0)
+      sorted = elem(acc,1)
+      if is_odd?(el) do
+        {res ++ [acc |> elem(1) |> hd], tl(sorted)}
+      else
+        {res ++ [el], sorted}
+      end
+    end) |> elem(0)
   end
 end
